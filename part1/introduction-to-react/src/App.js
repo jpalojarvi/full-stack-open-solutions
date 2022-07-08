d77e33863981e4940c25a7f671f91352b2bc57af
Johannes <johannes@jBook.local>
2022-07-08 11:26:21 +0300

    continuing course at part1, part 1.1 done

--- part1/introduction-to-react/src/App.js
+++ part1/introduction-to-react/src/App.js
@@ -1,8 +1,17 @@
-import React from 'react'
 const Hello = (props) => {
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
     </div>
   )
 }
@@ -16,7 +25,7 @@ const App = () => {
       <h1>Greetings</h1>
       <Hello name="Maya" age={26 + 10} />
       <Hello name={name} age={age} />
-      <Hello />
+      <Footer />
     </>
   )
 }
