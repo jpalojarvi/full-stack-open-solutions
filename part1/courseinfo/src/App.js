45c18351d6004e7a673dc19f0f30687eb45662fe
Johannes <johannes@jBook.local>
2022-07-08 16:41:32 +0300

    done part 1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -8,30 +8,38 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
-    <>
-    <p>{props.part1} {props.exercises1}</p>
-    <p>{props.part2} {props.exercises2}</p> 
-    <p>{props.part3} {props.exercises3}</p>
-    </>
+    <div>
+    <Part part1={props.part1} exercises1={props.exercises1}/>
+    <Part part2={props.part2} exercises2={props.exercises2}/>
+    <Part part3={props.part3} exercises3={props.exercises3}/>
+    </div>
   )
 }
 
 const Total = (props) => {
   return (
-  <>
   <p>{props.total}</p>
-  </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
+    <p>{props.part1}{props.exercises1}</p>
+    <p>{props.part2}{props.exercises2}</p>
+    <p>{props.part3}{props.exercises3}</p>
+    </>
   )
 }
 
 const App = () => {
   
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
+  const part1 = 'Fundamentals of React '
   const exercises1 = 10
-  const part2 = 'Using props to pass data'
+  const part2 = 'Using props to pass data '
   const exercises2 = 7
-  const part3 = 'State of a component'
+  const part3 = 'State of a component '
   const exercises3 = 14
   const total = 'Number of exercises: ' + (exercises1 + exercises2 + exercises3)
 
